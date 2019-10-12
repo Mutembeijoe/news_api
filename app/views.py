5a37caee55ca12746f028e0db15808d014abbf43
mutembeijoe <mutembeijoe@gmail.com>
2019-10-12 14:52:59 +0300

    replaces flask-bootstrap with flask-bootstrap4, uses boostrap classes to display news sources on the home page

--- app/views.py
+++ app/views.py
@@ -2,10 +2,13 @@ from app import app
 from flask import render_template
 from .requests import get_sources
 
-
-
 @app.route('/')
 def index():
     sources_list = get_sources()
     title = 'Sources'
+    print(sources_list[0].__dict__)
     return render_template('index.html', sources = sources_list, title= title)
+
+@app.route('/source/<name>')
+def source(name):
+    return f"<h1>Hello {name}</h1>"
